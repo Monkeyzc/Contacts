829370a8a5ee3c723b41877684b7a8c40772b7fd
Zhao Fei <zhaofei199210@gmail.com>
2017-02-18 11:46:58 +0800

    feat: add contact details file

---
+++ ZFContacts/Contacts/ContactDetails/View/ContactDetailsHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  ContactDetailsHeaderView.h
+//  ZFContacts
+//
+//  Created by zhaofei on 2017/2/18.
+//  Copyright © 2017年 ZhaoFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactDetailsHeaderView : UIView
+
+@end
---
+++ ZFContacts/Contacts/ContactDetails/View/ContactDetailsHeaderView.m
@@ -0,0 +1,18 @@
+//
+//  ContactDetailsHeaderView.m
+//  ZFContacts
+//
+//  Created by zhaofei on 2017/2/18.
+//  Copyright © 2017年 ZhaoFei. All rights reserved.
+//
+
+#import "ContactDetailsHeaderView.h"
+#import "Masonry.h"
+
+@interface ContactDetailsHeaderView()
+
+@end
+
+@implementation ContactDetailsHeaderView
+
+@end
