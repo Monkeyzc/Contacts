4ea46332d4d2ae03145b79b42cfdc5c38b8eb167
Zhao Fei <zhaofei199210@gmail.com>
2017-02-18 11:40:13 +0800

    refactor: regroup files

---
+++ ZFContacts/Contacts/ContactDetails/ContactDetailsController.m
@@ -0,0 +1,27 @@
+//
+//  ContactDetailsController.m
+//  ZFContacts
+//
+//  Created by zhaofei on 2017/2/18.
+//  Copyright © 2017年 ZhaoFei. All rights reserved.
+//
+
+#import "ContactDetailsController.h"
+
+@interface ContactDetailsController ()
+
+@end
+
+@implementation ContactDetailsController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
