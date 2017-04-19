cacfdd0ca5efa26597976e6f2bcbabfaa1e038d3
Zhao Fei <zhaofei199210@gmail.com>
2017-04-19 22:28:50 +0800

    fix(contact-model): lazy load

--- ZFContacts/Contacts/model/ContactModel.m
+++ ZFContacts/Contacts/model/ContactModel.m
@@ -13,41 +13,53 @@
 @implementation ContactModel
 
 - (NSString *)fullName {
-    if (self.firstName && self.lastName) {
-        return [NSString stringWithFormat:@"%@ %@", self.firstName, self.lastName];
-    } else if (self.firstName) {
-        return self.firstName;
-    } else if (self.lastName) {
-        return self.lastName;
-    } else {
-        return @"Contact-fullname";
+    if (!_fullName) {
+        if (self.firstName && self.lastName) {
+            _fullName = [NSString stringWithFormat:@"%@ %@", self.firstName, self.lastName];
+        } else if (self.firstName) {
+            _fullName = self.firstName;
+        } else if (self.lastName) {
+            _fullName = self.lastName;
+        } else {
+            _fullName = @"Contact-fullname";
+        }
     }
+    return _fullName;
 }
 
 - (NSString *)abbreviatedName {
-    if (self.firstName && self.lastName) {
-        return [NSString stringWithFormat:@"%@ %@", [self.firstName substringToIndex:1], [self.lastName substringToIndex:1]];
-    } else if (self.firstName) {
-        return [self.firstName substringToIndex:1];
-    } else if (self.lastName) {
-        return [self.lastName substringToIndex:1];
-    } else {
-        return @"#";
+    if (!_abbreviatedName) {
+        if (self.firstName.length && self.lastName.length) {
+            _abbreviatedName = [NSString stringWithFormat:@"%@ %@", [self.firstName substringToIndex:1], [self.lastName substringToIndex:1]];
+        } else if (self.firstName.length) {
+            _abbreviatedName = [self.firstName substringToIndex:1];
+        } else if (self.lastName.length) {
+            _abbreviatedName = [self.lastName substringToIndex:1];
+        } else {
+            _abbreviatedName = @"#";
+        }
     }
+    return _abbreviatedName;
 }
 
 - (NSString *)firstLetter {
-    HanyuPinyinOutputFormat *outputFormat=[[HanyuPinyinOutputFormat alloc] init];
-    [outputFormat setToneType:ToneTypeWithoutTone];
-    [outputFormat setVCharType:VCharTypeWithV];
-    [outputFormat setCaseType:CaseTypeLowercase];
-    
-    NSString *outputPinyin= [PinyinHelper toHanyuPinyinStringWithNSString:self.abbreviatedName withHanyuPinyinOutputFormat:outputFormat withNSString:@" "];
-    return [[outputPinyin substringToIndex:1] uppercaseString];
+    if (!_firstLetter) {
+        HanyuPinyinOutputFormat *outputFormat=[[HanyuPinyinOutputFormat alloc] init];
+        [outputFormat setToneType:ToneTypeWithoutTone];
+        [outputFormat setVCharType:VCharTypeWithV];
+        [outputFormat setCaseType:CaseTypeLowercase];
+        
+        NSString *outputPinyin= [PinyinHelper toHanyuPinyinStringWithNSString:self.abbreviatedName withHanyuPinyinOutputFormat:outputFormat withNSString:@" "];
+        _firstLetter = [[outputPinyin substringToIndex:1] uppercaseString];
+    }
+    return _firstLetter;
 }
 
 - (NSString *)searchText {
-    return [NSString stringWithFormat:@"%@%@%@", self.firstName ? self.firstName : @"", self.lastName ? self.lastName : @"", self.phoneNumber ? self.phoneNumber : @""];
+    if (!_searchText) {
+        _searchText = [NSString stringWithFormat:@"%@%@%@", self.firstName ? self.firstName : @"", self.lastName ? self.lastName : @"", self.phoneNumber ? self.phoneNumber : @""];
+    }
+    return _searchText;
 }
 
 //- (NSString *)description{
